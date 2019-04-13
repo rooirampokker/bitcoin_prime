1fd5a784b45ed17ab87e36a665c12bee838e6327
rooirampokker <rooirampokker@gmail.com>
2019-04-13 22:21:55 +0000

    successfully fetching historical data from coinapi and outputting to console

--- src/components/DatePickerComp.jsx
+++ src/components/DatePickerComp.jsx
@@ -32,6 +32,8 @@ class DatePickerComp extends Component {
                     <DatePicker
                         selected={this.state.thisDate}
                         onChange={this.handleDate}
+                        //2016-01-01T00:00:00
+                        dateFormat="YYYY/MM/dd"
                         id={this.id}
                         className='form-control'/>
                 </Col>
--- src/containers/Form.jsx
+++ src/containers/Form.jsx
@@ -11,38 +11,73 @@ import Moment from 'moment';
 class FormContainer extends Component {
     constructor(props) {
         super(props);
-        this.apiUrl             = 'https://rest.coinapi.io/';
+        this.apiUrl             = 'https://rest.coinapi.io';
+        this.currency           = 'BITSTAMP_SPOT_BTC_USD';
+        //GET
+        this.getTradePath       = '';
         this.setSelectedDate    = this.setSelectedDate.bind(this);
-        this.formSubmit          = this.formSubmit.bind(this);
+        this.formSubmit         = this.formSubmit.bind(this);
+        this.formData           =  new FormData();
         this.state = {
-            toDate: new Date(),
-            fromDate: new Date()
+            toDate: new Date().toISOString(),
+            fromDate: new Date().toISOString()
         }
     }
 /*
 *
 */
     setSelectedDate(selectedDate, dateType) {
-        this.setState({[dateType]: selectedDate});
+        this.setState({[dateType]: selectedDate.toISOString()});
     }
 /*
 *
 */
-    formSubmit(e) {
-        console.log(this.validateDateSelection())
-        // e.preventDefault();
-        // let formData = new FormData();
-        // let myHeaders = new Headers();
-        // fetch(this.apiUrl,
-        //       this.fetchParams(formData, myHeaders))
-        //     .then(response => response.json())
-        //     .then((response) => {
-        //        console.log(response);
-        //        this.setState({submitted: true});
-        //     })
+    buildFetchParams(RESTMethod='GET') {
+        let thisData  = new FormData();
+        let myHeaders = new Headers();
+        myHeaders.append('X-CoinAPI-Key', 'B2AA66AA-DAC5-401C-BEAF-BA70D0049792');
+        myHeaders.append('Accept', 'application/json,');
+        myHeaders.append('Accept-Encoding', 'deflate, gzip');
+        Object.keys(this.state).map(key => {
+            this.formData.append(this[key], this.state[key]);
+        });
+        //WORKS WELL FOR LOADING FORMDATA FOR 'POST'ing AND DISABLE BODY FOR GET
+        let thisBody = RESTMethod === 'GET' ? '' : {body: this.formData};
+
+
+        let returnParams = {
+                method: RESTMethod,
+                headers: myHeaders,
+                thisBody,
+            }
+        return returnParams;
+    }
+
+/*
+*  /v1/trades/BITSTAMP_SPOT_BTC_USD/history?time_start=2019-04-13T21:28:00.260Z&time_end=2019-04-13T21:28:00.260Z
+*/
+    getTradesOverPeriod() {
+        return this.apiUrl+"/v1/trades/"+this.currency+"/history?time_start="+this.state.fromDate+"&time_end="+this.state.toDate;
     }
 /*
 *
+ */
+    formSubmit(e) {
+        e.preventDefault();
+        if(this.validateDateSelection()) {
+            fetch(this.getTradesOverPeriod(),this.buildFetchParams())
+             .then(response => response.json())
+                .then((response) => {
+                    response.forEach(element => {
+                        console.log(element.timeExchange);
+                        console.log(element.price);
+                    });
+
+                });
+        }
+    }
+/*
+* //this must eventually pop up as toaster, modal or other integrated message
 */
     validateDateSelection() {
         let monthsApart = Moment(this.state.toDate).diff(Moment(this.state.fromDate), 'months', true);
@@ -50,7 +85,7 @@ class FormContainer extends Component {
         alert("please ensure that start and end dates are no more than 6 months apart");
         return false;
         } else {
-            alert("pass dates to API for processing...");
+            console.log("pass dates to API for processing...");
             return true;
         }
     }
